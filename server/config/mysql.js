b51c3edf348a923ec854eb9f60391b759a923681
Harvey Chui <chuiharvey@gmail.com>
2020-08-31 14:24:02 -0700

    Fixed login and turned it into async await

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -19,7 +19,7 @@ const pool = mysql.createPool({
 	port: '3306',
 	user: 'root',
 	password: password,
-	database: 'ocrFun',
+	database: 'ocrfun',
 	queryFormat: queryFormat
 });
 
--- server/controllers/auth/login.js
+++ server/controllers/auth/login.js
@@ -6,8 +6,7 @@ const bcrypt            = require("bcrypt"),
 
 module.exports = async (req, res) => {
   let query       = ``,
-      queryData   = [],
-      id          = '';
+      queryData   = [];
 
   // Expected login data
 	if (!req.body.email || !req.body.password)
@@ -24,26 +23,19 @@ module.exports = async (req, res) => {
   query = `SELECT HEX(id) id, passwordHash FROM users WHERE email = ? LIMIT 1`;
   queryData = [ req.body.email ];
 
-  promisePool.execute(query, queryData)
-    .then(data => {
-      if (data.length === 0)
-        throw { status: 400, message: "loginErr" };
-      id = data[0].id;
-      return bcrypt.compareAsync(req.body.password, data[0].passwordHash)
-    })
-    .then(isMatch => {
-      if (!isMatch)
-  			throw { status: 400, message: "loginErr" };
-        // Create a new jwt token for the user
-        const tsbToken = jwt.sign({
-          id: id,
-          exp: constants.expTime
-        }, serverKeys.jwtKey);
-        return res.status(200).json(tsbToken);
-    })
-    .catch(error => {
-      if (error.status)
+  try {
+    const [row] = await promisePool.execute(query, queryData);
+    if (row.length === 0) throw { status: 400, message: "loginErr" };
+    const isMatch = await bcrypt.compare(req.body.password, row[0].passwordHash);
+    if (!isMatch)	throw { status: 400, message: "loginErr" };
+    const tsbToken = jwt.sign({
+      id: row[0].id,
+      exp: constants.expTime
+    }, serverKeys.jwtKey);
+    return res.status(200).json(tsbToken);
+  } catch (err) {
+    if (error.status)
         return res.status(error.status).json({ message: error.message });
       return res.status(400).json({ message: "admin", error: error });
-    });
+  }
 }
