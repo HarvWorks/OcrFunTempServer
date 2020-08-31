f79a00b615dbd450d51a743756d2064218af2146
Harvey Chui <chuiharvey@gmail.com>
2020-08-31 12:26:11 -0700

    Init commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const   bp              = require('body-parser'),
+        express         = require('express'),
+        helmet          = require('helmet'),
+        expressJWT      = require('express-jwt'),
+        jwtKey          = require('./keys/keys').jwtKey,
+        path            = require('path'),
+        app             = express(),
+        port            = process.env.PORT || 3000;
+
+// Use expressJWT
+app.use('/api', expressJWT({ secret: jwtKey }));
+// Check token against redis database
+app.use('/api', (req, res, next) => serverFunctions.tokenChecker(req, res, next));
+
+const server = app.listen(port, function () {
+	console.log(`server running on port ${port}`);
+});
+
+app.use(helmet());
+app.use(bp.json());
+
+// This is for the front end, being able to route through them.
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Initialize the routes
+require('./server/config/routes.js')(app);
+
+app.use(function (err, req, res, next) {
+  console.log(err);
+  res.status(err.status || 500);
+  res.json({ message: "admin" , error: err.message});
+  next(err)
+})
